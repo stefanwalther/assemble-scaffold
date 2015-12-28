139e9b1fd6c347c4d0df18f27bbe8d8a181fb7f4
Stefan Walther <swr@qlik.com>
2015-12-28 11:13:32 +0100

    Re-factor tests a bit.

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -1,73 +1,76 @@
 "use strict";
-var AssembleScaffold = require("./../../lib/");
-var chai = require("chai");
+var AssembleScaffold = require( "./../../lib/" );
+var chai = require( "chai" );
 var expect = chai.expect;
-var path = require("path");
-var fs = require("fs");
+var path = require( "path" );
+var fs = require( "fs" );
+var mkdirp = require( "mkdirp" );
+var config = require("./config");
 
 var assembleScaffold = null;
 describe( "assemble-scaffold", function () {
 
 	beforeEach( function ( done ) {
-		assembleScaffold = new AssembleScaffold( require("./config"));
+		assembleScaffold = new AssembleScaffold( config );
 		assembleScaffold.runTask( "clean", function ( err ) {
-			done();
-		})
-	} );
-
-	it( "is an object", function ( ) {
-		expect( assembleScaffold ).to.be.an("object");
-	} );
-
-	it( "tasks are loaded from dir", function ( ) {
-		expect(assembleScaffold.app ).to.be.an("object");
-		expect(assembleScaffold.app.tasks.clean ).to.exist;
-		expect(assembleScaffold.app.tasks["content:pages"] ).to.exist;
-	} );
-
-	/**
-	 * @todo: Add some files before to check ...
-	 */
-	it( "cleans up the .build folder", function ( done ) {
-		assembleScaffold.runTask("clean", function ( err ) {
 			expect( err ).to.not.exist;
-			done();
-		})
+			done( err );
+		} )
+	} );
+
+	it( "is an object", function () {
+		expect( assembleScaffold ).to.be.an( "object" );
+	} );
+
+	it( "tasks are loaded from dir", function () {
+		expect( assembleScaffold.app ).to.be.an( "object" );
+		expect( assembleScaffold.app.tasks.clean ).to.exist;
+		expect( assembleScaffold.app.tasks["content:pages"] ).to.exist;
+	} );
+
+	it( "cleans up the .build folder", function ( done ) {
+		mkdirp( config.dest , {}, function ( err, made ) {
+			expect( err ).to.not.exist;
+			expect( made ).to.exist;
+			assembleScaffold.runTask( "clean", function ( err ) {
+				expect( err ).to.not.exist;
+				done();
+			} )
+		});
 	} );
 
 	it( "converts less files to a single .css file", function ( done ) {
-		return assembleScaffold.runTask("css", function ( err ) {
+		return assembleScaffold.runTask( "css", function ( err ) {
 			expect( err ).to.not.exist;
-			expect( fs.existsSync(path.join(__dirname, "./../.build/assets/css/main.css")) ).to.be.true;
+			expect( fs.existsSync( path.join( config.dest, "./assets/css/main.css" ) ) ).to.be.true;
 			done();
-		})
+		} )
 	} );
 
 	it( "does not convert imported css files", function ( done ) {
-		assembleScaffold.runTask("css", function ( err ) {
+		assembleScaffold.runTask( "css", function ( err ) {
 			expect( err ).to.not.exist;
-			expect( fs.existsSync(path.join(__dirname, "./../.build/assets/css/variables.css")) ).to.be.false;
+			expect( fs.existsSync( path.join( config.dest, "./assets/css/variables.css" ) ) ).to.be.false;
 			done();
-		});
+		} );
 	} );
 
 	it( "copies assets", function ( done ) {
-		assembleScaffold.runTask("assets", function ( err ) {
+		assembleScaffold.runTask( "assets", function ( err ) {
 			expect( err ).to.not.exist;
-			expect( fs.existsSync(path.join(__dirname, "./../.build/assets/data.json")) ).to.be.true;
-			expect( fs.existsSync(path.join(__dirname, "./../.build/assets/images/a.png")) ).to.be.true;
-			expect( fs.existsSync(path.join(__dirname, "./../.build/assets/images/sub/y.png")) ).to.be.true;
+			expect( fs.existsSync( path.join( config.dest, "./assets/data.json" ) ) ).to.be.true;
+			expect( fs.existsSync( path.join( config.dest, "./assets/images/a.png" ) ) ).to.be.true;
+			expect( fs.existsSync( path.join( config.dest, "./assets/images/sub/y.png" ) ) ).to.be.true;
 			done();
-		});
+		} );
 	} );
 
-
-
-	it( "runs", function ( done ) {
+	// Todo: Write this test at the end
+	it.skip( "runs", function ( done ) {
 		assembleScaffold.run( function ( err ) {
 			expect( err ).to.not.exist;
 			done();
-		});
+		} );
 	} );
 
 } );
